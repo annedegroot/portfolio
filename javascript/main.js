07f70a496fe3bf474f84a210299f8baae016c88d
annedegroot <annedegroot95@hotmail.com>
2017-01-04 19:52:37 +0100

    1e upload
    Alle bestanden geupload

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+/* Bron: http://www.w3schools.com/howto/howto_js_sidenav.asp */
+
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
